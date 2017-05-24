908e27b8529be83e6623cf533b8878910993b76e
drosofff <drosofff@gmail.com>
2017-05-24 10:23:44 +0200

    initial implementation of py36 Repenrich

---
+++ py36/tests/install_conda_env.sh
@@ -0,0 +1,10 @@
+
+wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+bash Miniconda2-latest-Linux-x86_64.sh
+export PATH="~/miniconda2/bin:$PATH"
+source ~/.bashrc
+conda --version
+echo "channels:" > ~/.condarc
+echo "  - bioconda" >> ~/.condarc
+echo "  - r" >> ~/.condarc
+echo "  - defaults" >> ~/.condarc
